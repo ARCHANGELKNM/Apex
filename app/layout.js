d667d863b8bab50f4539b4f21f56178b55e1a74e
ARCHANGELKNM <kanyamuraarchangel644@gmail0.com>
2026-08-23 17:31:16 +0200

    Added Google Adsense Meta tag

--- app/layout.js
+++ app/layout.js
@@ -5,10 +5,18 @@ import React from "react";
 import "./globals.css";
 
 export default function RootLayout({ children }) {
+
+
   return (
     <html lang="en">
-      <body className="antialiased bg-[#F9F6EE] text-black min-h-screen selection:bg-yellow-300 font-sans overflow-x-clip w-full max-w-full min-w-0">
-        {children}
+      <body className="antialiased bg-[#F9F6EE] text-black min-h-screen selection:bg-yellow-300 font-sans">
+        <meta name="google-adsense-account" content="ca-pub-8679808720592276">
+          <main>
+            <div className="w-full max-w-full overflow-x-hidden min-w-0 px-4 sm:px-6 md:px-8 mx-auto">
+              {children}
+            </div>
+          </main>
+        </meta>
       </body>
     </html>
   );
