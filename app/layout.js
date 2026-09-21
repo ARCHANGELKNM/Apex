6cf0079ee660ff4e83be95fe0f9ee571ef88de6c
ARCHANGELKNM <kanyamuraarchangel644@gmail0.com>
2026-09-21 03:22:16 +0200

    refactor(ads): remove unused advertisement components and scripts

--- app/layout.js
+++ app/layout.js
@@ -1,20 +1,13 @@
 "use client";
 
 import React from "react";
-import { PublisherSocialBar } from "@/components/ads/PublisherAds";
 
 import "./globals.css";
 
 export default function RootLayout({ children }) {
-
-
   return (
     <html lang="en">
-      <head>
-        <meta name="monetag" content="0e82f58084b4d74dd8c96a710c005f62" />
-      </head>
       <body className="antialiased bg-[#F9F6EE] text-black min-h-screen selection:bg-yellow-300 font-sans">
-        <PublisherSocialBar />
         <main>
           <div className="w-full max-w-full overflow-x-hidden min-w-0 px-4 sm:px-6 md:px-8 mx-auto">
             {children}
